947b5cde27b7901f4de830c2ad674448a7f648e7
luciferhunmain <54854974+luciferhunmain@users.noreply.github.com>
2021-04-18 23:16:20 +0530

    Update tools.py

--- userbot/plugins/tools.py
+++ userbot/plugins/tools.py
@@ -361,7 +361,8 @@ async def _(event):
     if event.fwd_from:
         return
     await edit_or_reply(event, "`Calculating the Length. Hang on`")
-    input = event.pattern_match.group(1)
+    inpu = await event.get_reply_message()
+    input = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
     input_str = str(input)
     length = len(input_str)
     if length == 0:
