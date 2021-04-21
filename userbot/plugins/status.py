67da375636593d364f7ac57fd56a054509b51ee1
luciferhunmain <54854974+luciferhunmain@users.noreply.github.com>
2021-04-21 23:46:13 +0530

    Update status.py

--- userbot/plugins/status.py
+++ userbot/plugins/status.py
@@ -4,16 +4,15 @@ import urllib
 from telethon.tl import functions
 
 OFFLINE_TAG = "[OFFLINE]"
-
+user_it = "me"
+user = await event.client.get_entity(user_it)
+original_first_name = user.first_name
+original_last_name = user.last_name
 
 @bot.on(admin_cmd(pattern="offline"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
-    user_it = "me"
-    user = await event.client.get_entity(user_it)
-    user.first_name
-    user.last_name
     if user.first_name.startswith(OFFLINE_TAG):
         await event.edit("**Already in Offline Mode.**")
         return
@@ -36,8 +35,12 @@ async def _(event):
         os.system("rm -fr donottouch.jpg")
     except Exception as e:  # pylint:disable=C0103,W0703
         logger.warn(str(e))  # pylint:disable=E0602
-    first_name = [user.first_name]
-    last_name = OFFLINE_TAG
+    if original_first_name startswith [:
+    first_name = original_first_name
+    last_name = original_last_name OFFLINE_TAG
+    else:
+    first_name = [original_first_name
+    last_name = original_last_name] OFFLINE_TAG
     try:
         await event.client(
             functions.account.UpdateProfileRequest(  # pylint:disable=E0602
@@ -54,8 +57,6 @@ async def _(event):
 async def _(event):
     if event.fwd_from:
         return
-    user_it = "me"
-    user = await event.client.get_entity(user_it)
     if user.first_name.startswith(OFFLINE_TAG):
         await event.edit("**Changing Profile to Online...**")
     else:
