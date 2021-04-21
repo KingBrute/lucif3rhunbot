d87b20597f7e9a97a670cf962531e9f7d91486b5
luciferhunmain <54854974+luciferhunmain@users.noreply.github.com>
2021-04-21 23:02:30 +0530

    Create status.py

---
+++ userbot/plugins/status.py
@@ -0,0 +1,99 @@
+import os
+import urllib
+
+from telethon.tl import functions
+
+OFFLINE_TAG = "[OFFLINE]"
+
+
+@bot.on(admin_cmd(pattern="offline"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    user_it = "me"
+    user = await event.client.get_entity(user_it)
+    if user.first_name.startswith(OFFLINE_TAG):
+        await event.edit("**Already in Offline Mode.**")
+        return
+    await event.edit("**Changing Profile to Offline...**")
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):  # pylint:disable=E0602
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)  # pylint:disable=E0602
+    urllib.request.urlretrieve(
+        "https://telegra.ph/file/249f27d5b52a87babcb3f.jpg", "donottouch.jpg"
+    )
+    photo = "donottouch.jpg"
+    if photo:
+        file = await event.client.upload_file(photo)
+        try:
+            await event.client(functions.photos.UploadProfilePhotoRequest(file))
+        except Exception as e:  # pylint:disable=C0103,W0703
+            await event.edit(str(e))
+        else:
+            await event.edit("**Changed profile to OffLine.**")
+    try:
+        os.system("rm -fr donottouch.jpg")
+    except Exception as e:  # pylint:disable=C0103,W0703
+        logger.warn(str(e))  # pylint:disable=E0602
+    last_name = user.first_name
+    first_name = OFFLINE_TAG
+    try:
+        await event.client(
+            functions.account.UpdateProfileRequest(  # pylint:disable=E0602
+                last_name=last_name, first_name=first_name
+            )
+        )
+        result = "**`{} {}`\nI am Offline now.**".format(first_name, last_name)
+        await event.edit(result)
+    except Exception as e:  # pylint:disable=C0103,W0703
+        await event.edit(str(e))
+
+
+@bot.on(admin_cmd(pattern="online"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    user_it = "me"
+    user = await event.client.get_entity(user_it)
+    if user.first_name.startswith(OFFLINE_TAG):
+        await event.edit("**Changing Profile to Online...**")
+    else:
+        await event.edit("**Already Online.**")
+        return
+    try:
+        await event.client(
+            functions.photos.DeletePhotosRequest(
+                await event.client.get_profile_photos("me", limit=1)
+            )
+        )
+    except Exception as e:  # pylint:disable=C0103,W0703
+        await event.edit(str(e))
+    else:
+        await event.edit("**Changed profile to Online.**")
+    first_name = user.last_name
+    last_name = ""
+    try:
+        await event.client(
+            functions.account.UpdateProfileRequest(  # pylint:disable=E0602
+                last_name=last_name, first_name=first_name
+            )
+        )
+        result = "**`{} {}`\nI am Online !**".format(first_name, last_name)
+        await event.edit(result)
+    except Exception as e:  # pylint:disable=C0103,W0703
+        await event.edit(str(e))
+
+CMD_HELP.update(
+    {
+        "status": """**Plugin : ** `Status`
+
+  •  **Syntax : **`.offline`
+  •  **Function : **__ Adds an offline tag in your name and change profile pic to black. __
+
+  •  **Syntax : **`.online`
+  •  **Function : **__ Remove Offline Tag from your name and change profile pic. __
+
+**Note - If you have a last name remove it unless it automatically removed.**
+
+
+}
+)
