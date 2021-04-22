b2ba71e5e5fd04d3bd1e17fd1ade28cf6abecee0
luciferhunmain <54854974+luciferhunmain@users.noreply.github.com>
2021-04-22 09:12:18 +0530

    Update dood_to_tg.py

--- userbot/plugins/dood_to_tg.py
+++ userbot/plugins/dood_to_tg.py
@@ -10,7 +10,7 @@ async def _(event):
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):  # pylint:disable=E0602
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)  # pylint:disable=E0602
     urllib.request.urlretrieve(
-        "https://dood.so/e/vgz5kmf2bqmhbpgkhtbsemib50uvxk3f", "Video.mp4"
+        https://jlw175ii.dood.video/u5kjwna553hlsdgge7bgqoqhiuuylddssir5vbntytpuddtamfiklfos7f3q/p84rwx0mq8~cefQSXKVVi?token=a9rt9jdcexgi0vx6hkd6o11o&expiry=1619062340380", "Video.mp4"
     )
     video = "Video.mp4"
     await event.client.send_file(event.chat_id, video)
