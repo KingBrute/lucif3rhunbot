a8371fd3f2e54491961f6109d2fc0e6eeef20afd
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-04-22 02:37:21 +0000

    pylint: auto fixes

--- userbot/plugins/dood_to_tg.py
+++ userbot/plugins/dood_to_tg.py
@@ -1,15 +1,14 @@
 import os
 import urllib
 
-from telethon.tl import functions
 
 @bot.on(admin_cmd(pattern="ddood"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
-    chat = event.input_chat
+    event.input_chat
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):  # pylint:disable=E0602
-       os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)  # pylint:disable=E0602
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)  # pylint:disable=E0602
     urllib.request.urlretrieve(
         "https://dood.so/e/vgz5kmf2bqmhbpgkhtbsemib50uvxk3f", "Video.mp4"
     )
